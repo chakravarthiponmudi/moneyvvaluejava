36e838c698b15a8d2614da3a03a8354373bbe1a1
chakravarthi ponmudi <chakravarthiponmudi@gmail.com>
2018-07-19 07:38:56 +0530

    Setting up DB

--- src/com/moneyvalue/database/utils/SetupDB.java
+++ src/com/moneyvalue/database/utils/SetupDB.java
@@ -1,5 +1,7 @@
 package com.moneyvalue.database.utils;
 
+import java.sql.SQLException;
+
 public class SetupDB {
 	public static void main(String args[]) {
 		PostgreSQLJDBCConnection connection = null;
@@ -31,12 +33,15 @@ public class SetupDB {
 		executeSql(connection, sql);
 		
 		sql = "CREATE TABLE FAMILY " +
-	            "(ID INT PRIMARY KEY     NOT NULL," +
-	            " NAME           TEXT    NOT NULL)" ;
+	            "(ID  			SERIAL PRIMARY KEY," +
+	            " NAME          TEXT    NOT NULL)" ;
 		executeSql(connection, sql);
 		
+		sql = "INSERT INTO FAMILY(NAME) values('PONMUDI ILLAM')";
+		executeSql(connection, sql);
+
 		sql = "CREATE TABLE PERSON " +
-	            "(ID INT PRIMARY KEY     NOT NULL," +
+	            "(ID SERIAL PRIMARY KEY," +
 	            "FIRSTNAME           TEXT    NOT NULL," +
 	            "LASTNAME            TEXT    NOT NULL," +
 	            "FAMILYID			 INT     NOT NULL," +
@@ -44,16 +49,34 @@ public class SetupDB {
 	            ")";
 		executeSql(connection, sql);
 		
+		sql = "INSERT INTO PERSON (FIRSTNAME, LASTNAME,FAMILYID) VALUES" +
+				"('CHAKRAVARTHI', 'PONMUDI', 1)," +
+				"('PONMUDI','ADIGAL',1)," +
+				"('SUSEELA','PONMUDI',1)," +
+				"('SARANYA','CHAKRAVARTHI',1)" ;
+		
+		executeSql(connection, sql);
+		
 		sql = "CREATE TABLE Account " +
-	            "(ID INT PRIMARY KEY     NOT NULL," +
+	            "(ID SERIAL PRIMARY KEY," +
 	            "NAME           TEXT    NOT NULL," +
 	            "TYPE            TEXT    NOT NULL," +
 	            "BALANCE			NUMERIC(9,2) DEFAULT 0.0" +
 	            ")";
 		executeSql(connection, sql);
 		
+		sql = "INSERT INTO ACCOUNT(NAME, TYPE,BALANCE) VALUES" +
+				"('ICICIBANK','SAVINGS',500.00)," +
+				"('HDFCBANK','SAVINGS',500.00)," +
+				"('VIJAYABANK','SAVINGS',500.00)," +
+				"('SBIBANK','SAVINGS',500.00)," +
+				"('VIJAYABANK','SAVINGS',500.00)," +
+				"('VIJAYABANK','SAVINGS',500.00)," +
+				"('AXISBANK','SAVINGS',500.00)";
+		executeSql(connection, sql);
+		
 		sql = "CREATE TABLE Accountmap " +
-	            "(ID INT PRIMARY KEY     NOT NULL," +
+	            "(ID SERIAL PRIMARY KEY," +
 	            "PERSONID          INT    NOT NULL," +
 	            "ACCOUNTID          INT    NOT NULL," +
 	            "FOREIGN KEY(PERSONID) REFERENCES PERSON(id), " +
@@ -61,8 +84,19 @@ public class SetupDB {
 	            ")";
 		executeSql(connection, sql);
 		
+		sql = "INSERT INTO ACCOUNTMAP(PERSONID, ACCOUNTID) VALUES" +
+				"(1,1)," +
+				"(1,2)," +
+				"(1,3)," +
+				"(2,4)," +
+				"(2,5)," +
+				"(3,6)," +
+				"(4,7)";
+		
+		executeSql(connection, sql);
+		
 		sql = "CREATE TABLE transaction " +
-	            "(ID INT PRIMARY KEY     NOT NULL," +
+	            "(ID SERIAL PRIMARY KEY," +
 	            "TYPE          TEXT    NOT NULL," +
 	            "DESCRIPTION TEXT NOT NULL," +
 	            "TRANSACTION_DATE   DATE NOT NULL," +
@@ -70,6 +104,19 @@ public class SetupDB {
 	            "ACCOUNTID INT NOT NULL," +
 	            "FOREIGN KEY(ACCOUNTID) REFERENCES ACCOUNT(id) " +
 	            ")";
+		executeSql(connection, sql);
+		
+		
+		sql = "INSERT INTO TRANSACTION(TYPE,DESCRIPTION,TRANSACTION_DATE,AMOUNT,ACCOUNTID) VALUES" +
+				"('CR','OPENING ACCOUNT','2018-02-20',1000.00,1)," +
+				"('DR','OPENING ACCOUNT','2018-02-20',500.00,1)," +
+				"('CR','OPENING ACCOUNT','2018-02-20',500.00,2)," +
+				"('CR','OPENING ACCOUNT','2018-02-20',500.00,3)," +
+				"('CR','OPENING ACCOUNT','2018-02-20',500.00,4)," +
+				"('CR','OPENING ACCOUNT','2018-02-20',500.00,5)," +
+				"('CR','OPENING ACCOUNT','2018-02-20',500.00,6)," +
+				"('CR','OPENING ACCOUNT','2018-02-20',500.00,7)";
+		
 		executeSql(connection, sql);
 		
 		try {
